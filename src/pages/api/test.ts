5d5e462d6a2aa1587976c3c644c051c14eaffee2
joel.barba <joel.barba.vidal@gmail.com>
2025-06-13 13:11:45 +0200

    add json file to store date

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -7,30 +7,50 @@ type Data = {
   content: string;
 };
 
-const dbFile = './data.js';
+const dbFile = './data.json';
 
 
 // curl "https://jb-api.netlify.app/api/test"
-export function GET(req: NextApiRequest, res: NextApiResponse<Data>) {
+// curl -X POST "https://jb-api.netlify.app/api/test" --data '{"username":"xyz","password":"xyz"}'
 
-  const content = JSON.parse(fs.readFileSync(dbFile, 'utf8')); 
+// curl -X GET "http://localhost:3000/api/test"
+// curl -X POST "http://localhost:3000/api/test" -d 'param1=hello&param2=world'
 
-  res.status(200).json({ 
-    name: 'Test API Works',
-    content,
-  });
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  console.log('----------------------------------------');
+
+  if (!fs.existsSync(dbFile)) {
+    fs.writeFileSync(dbFile, JSON.stringify({ var1: 'empty' }, null, 2));
+  }
+
+
+  if (req.method === 'GET') {
+    console.log('GET request received');
+      const content = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
+      res.status(200).json({ 
+        name: 'Test API Works',
+        content,
+      });
+
+  }
+  else if (req.method === 'POST') {
+    console.log('POST request received');
+    console.log('BODY = ', req.body);
+    
+    const content = JSON.parse(fs.readFileSync(dbFile, 'utf8'));
+    content.var2 = req.body.param1 || 'default value';
+    content.var3 = req.body.param2 || 'default value';
+
+    fs.writeFileSync(dbFile, JSON.stringify(content, null, 2));
+    
+
+    res.status(200).json({ 
+      name: 'Test API Works',
+      content,
+    });
+
+  }
+
 }
 
-
-// curl -X POST "https://jb-api.netlify.app/api/test"
-export function POST(req: NextApiRequest, res: NextApiResponse<Data>) {
-
-  fs.writeFileSync(dbFile, JSON.stringify({ var1: 'initial data' }, null, 2));
-  
-  const content = JSON.parse(fs.readFileSync('./data.js', 'utf8')); 
-
-  res.status(200).json({ 
-    name: 'Test API Works',
-    content,
-  });
-}
